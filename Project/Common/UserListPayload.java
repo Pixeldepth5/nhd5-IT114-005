a2a1e2876890c098882c1f122e50441c446f2810
Nilkanth dhariya <nhd5@njit.edu>
2025-12-08 12:38:11 -0500

    Updated userlistpayload.java

--- Project/Common/UserListPayload.java
+++ Project/Common/UserListPayload.java
@@ -1,6 +1,6 @@
 // UCID: nhd5
 // Date: December 7, 2025
-// Description: TriviaGuessGame UserListPayload – sends user state to all clients
+// Description: TriviaGuessGame UserListPayload – syncs the user list / status to all clients
 
 package Common;
 
@@ -9,25 +9,22 @@ import java.util.ArrayList;
 public class UserListPayload extends Payload {
 
     private ArrayList<Long> clientIds = new ArrayList<>();
-    private ArrayList<String> names = new ArrayList<>();
+    private ArrayList<String> displayNames = new ArrayList<>();
     private ArrayList<Integer> points = new ArrayList<>();
     private ArrayList<Boolean> lockedIn = new ArrayList<>();
     private ArrayList<Boolean> away = new ArrayList<>();
     private ArrayList<Boolean> spectator = new ArrayList<>();
 
-    public void addUser(long id, String name, int pts, boolean isLocked, boolean isAway, boolean isSpectator) {
-        clientIds.add(id);
-        names.add(name);
-        points.add(pts);
-        lockedIn.add(isLocked);
-        away.add(isAway);
-        spectator.add(isSpectator);
-    }
-
     public ArrayList<Long> getClientIds() { return clientIds; }
-    public ArrayList<String> getDisplayNames() { return names; }
+    public ArrayList<String> getDisplayNames() { return displayNames; }
     public ArrayList<Integer> getPoints() { return points; }
     public ArrayList<Boolean> getLockedIn() { return lockedIn; }
     public ArrayList<Boolean> getAway() { return away; }
     public ArrayList<Boolean> getSpectator() { return spectator; }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+               String.format(" Users[%d]", clientIds.size());
+    }
 }
